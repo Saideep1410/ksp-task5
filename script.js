748295f6b3302c91ca801d8bd9a9e7ce9b2879a5
Saideep1410 <113224570+Saideep1410@users.noreply.github.com>
2023-12-12 11:49:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("hello world");
+
+let number1=20;
+let number2=10;
+let number3=number1+number2;
+console.log(number3);
+
+let result=((number1+number2)*number3);
+
+console.log(result);
+
+console.log(++number2);
+console.log(--number1);
+
+console.log(number1*number2);
+console.log(number1-number2);
+console.log(number2/number1);
+console.log(number1**number2);
+console.log(number1%number2);
+
+console.log(number1==number2);
+console.log(number1<number2);
+console.log(number1>number2);
+console.log(number1=number2);
+console.log(number1===number2);
+console.log(number1!=nimber2);
